a49fcfa559d4b811ada5599606d15f1cc44a073b
Tomasz Kruczek <t.kruczek.pl@gmail.com>
2011-11-06 13:00:54 +0100

    tabController

--- js-test/tabController-test.js
+++ js-test/tabController-test.js
@@ -1,39 +1,39 @@
-(function (){
-	var tabController = tddjs.ui.tabController;
-	
-	//All test cases can share this setUp
-	function setUp() {
-		/*: DOC += <ol id = "tabs">
-		 * <li><a href="#news">News</a></li>
-		 * <li><a href="#sports">Sports</a></li>
-		 * <li><a href="#economy">Economy</a></li>
-		 */
-		this.tabs = document.getElementById('tabs');
-	}
-	
-	TestCase('TabControllerCreateTest', {
-		setUp: setUp,
-		
-		'test should fail without element' : function() {
-			assertException(function() {
-				tabController.create();
-			}, 'TypeError');
-		},
-		
-		'test should fail without element class' : function() {
-			assertException(function() {
-				tabController.create({});
-			}, 'TypeError');
-		},
-		
-		'test should return object' : function() {
-			var controller = tabController.create(this.tabs);
-			assertObejct(contoller);
-		},
-		
-		'test should add js-tabs class name to element' : function() {
-			var tabs = tabController.create(this.tabs);
-			assertClassName('js-tab-controller', this.tabs);
-		}
-	});
+/*jslint indent: 2, browser: true*/
+/*globals TestCase, tddjs, assertException, assertObject, assertClassName, assertNoMatch, assertNoException, assertSame*/
+
+(function () {
+  var tabController = tddjs.ui.tabController;
+
+  //All test cases can share this setUp
+  function setUp() {
+    /*: DOC += <ol id = "tabs">
+     * <li><a href="#news">News</a></li>
+     * <li><a href="#sports">Sports</a></li>
+     * <li><a href="#economy">Economy</a></li>
+     */
+    this.tabs = document.getElementById('tabs');
+  }
+
+  TestCase('TabControllerCreateTest', {
+    setUp : setUp,
+
+    'test should fail without element' : function () {
+      assertException(function () {
+        tabController.create();
+      }, 'TypeError');
+    },
+    'test should fail without element class' : function () {
+      assertException(function () {
+        tabController.create({});
+      }, 'TypeError');
+    },
+    'test should return object' : function () {
+      var controller = tabController.create(this.tabs);
+      assertObject(controller);
+    },
+    'test should add js-tabs class name to element' : function () {
+      var tabs = tabController.create(this.tabs);
+      assertClassName('js-tab-controller', this.tabs);
+    }
+  });
 }());
